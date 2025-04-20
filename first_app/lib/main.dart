b662c47c2dc00585ffaad046047f38d00348a2d8
Abu Bakar Istiak <abubakar119147@gmail.com>
2025-04-20 12:20:34 +0600

    added function

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -4,15 +4,20 @@ void main() {
   int age = 20;
   String name =  "Abu Bakar Istiak";
   String position = "Flutter developer & Competitive Programmer";
+  int salary = add(20,10);
   runApp(
     MaterialApp(
       home: Scaffold(
         body: Center(
           child: Text(
-            "Hello world - I am $name ($position). I am $age years old.",
+            "Hello world - I am $name ($position). I am $age years old. Salary Range $salary thousand.",
           ),
         ),
       ),
     ),
   );
+}
+
+int add(int salary, int bonus){
+  return salary+bonus;
 }
